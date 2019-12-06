26e2e68e993a9f1bb3a279a1de1a83ab0c43e82f
dminko <mitko@experta.bg>
2019-12-06 18:59:56 +0200

    Отделена ф-я

--- em4100Decode.php
+++ em4100Decode.php
@@ -17,11 +17,10 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 
 
-$rfid[] = "26 16 45 5F 02 60 A4 FF"; //  4695336 или 071,42280
-$rfid[] = "AE 03 45 5F 02 60 A4 FF"; //  4695310 или 071,42254
-$rfid[] = "F4 16 2A 25 05 00 98 FF"; // 11115051 или 169,39467
+// $rfid[] = "26 16 45 5F 02 60 A4 FF"; //  4695336 или 071,42280
+// $rfid[] = "AE 03 45 5F 02 60 A4 FF"; //  4695310 или 071,42254
+$rfid = "F4 16 2A 25 05 00 98 FF"; // 11115051 или 169,39467
 
-$resI = array();
 
 /***
 /* return array
@@ -45,38 +44,42 @@ function reverseOrder($input)
     
     return $res;
 }
-foreach ($rfid as $rf) {
-    $resI[] = reverseOrder($rf);
-}
 
-$bit64 = '';
-foreach ($resI as $res) {
+
+/***
+/* return int
+***/
+function decode($rfid) {
+    
+    $resArr = array();
+    
+    $resArr = reverseOrder($rfid);
+    
     $bit64 = '';
-    foreach ($res as $byte) {
+    foreach ($resArr as $byte) {
         $bit64 .= str_pad(decbin(hexdec($byte)), 8, '0', STR_PAD_LEFT);
     }
-    $bit64Arr[] = $bit64;
-}
-
-$dataBits = array(	20,21,22,23, 
-                    25,26,27,28,
-                    30,31,32,33,
-                    35,36,37,38,
-                    40,41,42,43,
-                    45,46,47,48,
-                    50,51,52,53,
-                    55,56,57,58
-                );
-
-$data = array();
-
-foreach ($bit64Arr as $binRes) {
+    
+    $dataBits = array(	20,21,22,23, 
+                        25,26,27,28,
+                        30,31,32,33,
+                        35,36,37,38,
+                        40,41,42,43,
+                        45,46,47,48,
+                        50,51,52,53,
+                        55,56,57,58
+                    );
+    
+    $data = array();
+    
     foreach ($dataBits as $bit) {
-        $data[$binRes] .= $binRes{$bit-1};
+        $data[$bit64] .= $bit64{$bit-1};
+        $dig .= $bit64{$bit-1};
     }
+    $data[$dig] = (int) $dig;
+    
+    return $data;
 }
 
-print_r ($data);
-
-exit(0);
+print_r (decode($rfid));
 
