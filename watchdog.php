d18b5beb0aa8c29e9a97060f8fb550e7d8130504
Dimitar Minekov <mitko@experta.bg>
2021-06-21 15:44:55 +0300

    bugfix

--- watchdog.php
+++ watchdog.php
@@ -1,10 +1,13 @@
 <?php
 exec("ps -A | grep php", $output, $returnVar);
 $cnt = 0;
+$pid = getmypid();
 foreach ($output as $res) {
-	if (strpos($res,'?') && strpos($res, '00:00:00')) {
-		$cnt++;
-	}
+    if (strpos($res, "{$pid}") === false) {
+        if (strpos($res,'?') && strpos($res, '00:00:00')) {
+            $cnt++;
+        }
+    }
 }
 if ($cnt < 2) {
 	file_put_contents('debug.log', date("Y-m-d H:i:s") . " - Restart ... \n", FILE_APPEND);
@@ -14,7 +17,7 @@ if ($cnt < 2) {
 	// echo ("ne restartirame\n");
 }
 
-
+exit (0);
 
 // print_r ($output);
 // echo ($returnVar);
