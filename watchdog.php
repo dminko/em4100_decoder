e7ef3c9e40a19922a1dd2210e6251bd8a8bbd1fb
Dimitar Minekov <mitko@experta.bg>
2021-06-21 16:50:00 +0300

    по добра логика

--- watchdog.php
+++ watchdog.php
@@ -1,18 +1,17 @@
 <?php
 exec("ps -A | grep php", $output, $returnVar);
-$cnt = 0;
-$pid = getmypid();
+$cntOK = 0;
+$cntAll = count($output);
+
 foreach ($output as $res) {
-    if (strpos($res, "{$pid}") === false) {
-        if (strpos($res,'?') && strpos($res, '00:00:00')) {
-            $cnt++;
-        }
+    if (strpos($res,'?') && strpos($res, '00:00:00')) {
+        $cntOK++;
     }
 }
-if ($cnt < 2) {
+if ($cntOK != $cntAll) {
 	file_put_contents('debug.log', date("Y-m-d H:i:s") . " - Restart ... \n", FILE_APPEND);
 	exec ('sudo reboot');
-} elseif ($cnt ==2) {
+} else {
 	// ne restartirame
 	// echo ("ne restartirame\n");
 }
